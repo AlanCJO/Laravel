e3b5265119294dece01859d65891b6c0651ac804
Alan Oliveira <alancjobm@gmail.com>
2021-02-01 21:56:07 -0300

    agrupando rotas

--- routes/web.php
+++ routes/web.php
@@ -23,9 +23,12 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 Route::get('/contato', 'ContatoController@contato');
 
-Route::get('/login', function(){ return 'Login'; });
-Route::get('/clientes', function(){ return 'Clientes'; });
-Route::get('/fornecedores', function() { return 'Fornecedores'; });
+Route::prefix('/app')->group(function() {
+    Route::get('/login', function(){ return 'Login'; });
+    Route::get('/clientes', function(){ return 'Clientes'; });
+    Route::get('/fornecedores', function() { return 'Fornecedores'; });
+});
+
 
 // Exemplos de parametrização e regex
 /*
