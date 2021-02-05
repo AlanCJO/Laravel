145f59320f92217511a95bf7077bc3f1b4cbeffa
Alan Oliveira <alancjobm@gmail.com>
2021-02-04 22:03:49 -0300

    unless

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,4 +1,4 @@
-<h3>Fornecedor</h3>
+<h4>Fornecedor</h4>
 
 
 {{ 'Teste' }}
@@ -12,12 +12,23 @@
 
 <br>
 
+@php 
+    /*
+        if(!<condicao>) {} // enquanto executa se o retorno for true
+    */
+@endphp
+
+{{-- @unless executa se o retorno for false --}}
+
 {{-- @dd($fornecedores) --}}
 
-@if(count($fornecedores) > 0 && count($fornecedores) < 10)
-    <h3>Existem alguns fornecedores cadastrados</h3>
-@elseif(count($fornecedores) > 10)
-    <h3>Existem vários fornecedores cadastrados</h3>
-@else 
-    <h3>Ainda não existem fornecedores cadastrados</h3>
-@endif
+<h4>Fornecedor: {{ $fornecedores[0]['nome'] }} </h4>
+<h4>Status: {{ $fornecedores[0]['status'] }} </h4>
+
+{{-- @if( !($fornecedores[0]['status'] == 'S') )
+    <p>Fornecedor Inativo</p>
+@endif --}}
+
+@unless($fornecedores[0]['status'] == 'S')
+    <p>Fornecedor Inativo</p>
+@endunless
