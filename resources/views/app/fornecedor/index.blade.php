6bfb2abc9fd0bd604a70247ee61a18d35a74fcb7
Alan Oliveira <alancjobm@gmail.com>
2021-02-10 23:45:03 -0300

    @for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -1,16 +1,5 @@
 <h4>Fornecedor</h4>
 
-{{ 'Teste' }}
-<?= 'Teste' ?> 
-
-{{-- Comentário no blade --}}
-
-@php 
-    echo 'Teste';
-@endphp
-
-<br>
-
 @php 
     /*
         if(empty($variavel)) {} // retornar true se a variável estiver vazia
@@ -25,60 +14,16 @@
     */
 @endphp
 
-
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }} 
-    <br>
-    Status: {{ $fornecedores[1]['status'] }}
-    <br>
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-    <br>
-    Telefone: ({{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }})
 
-    @switch($fornecedores[0]['ddd'])
-        @case('11')
-            São Paulo - SP 
-            @break
-        @case('32')
-            Juiz de Fora - MG
-            @break
-        @case('85')
-            Fortaleza - CE
-            @break
-        @default
-            Estado não identificado
-        @endswitch
-
+    @for($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[$i]['nome'] }} 
+        <br>
+        Status: {{ $fornecedores[$i]['status'] }}
+        <br>
+        CNPJ: {{ $fornecedores[$i]['cnpj'] ?? '' }}
+        <br>
+        Telefone: ({{ $fornecedores[$i]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }})
+        <hr>
+    @endfor
 @endisset
-
-@unless($fornecedores[0]['status'] == 'S')
-<p>Fornecedor Inativo</p>
-@endunless
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-{{-- @if( !($fornecedores[0]['status'] == 'S') )
-    <p>Fornecedor Inativo</p>
-@endif --}}
-
-{{-- @unless executa se o retorno for false --}}
-
-{{-- @dd($fornecedores) --}}
